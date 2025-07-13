33e48b6c935c72cad2dd80bf876744a195f67d53
Abhay Mahto <abhaymahto0533@gmail.com>
2025-07-13 13:11:52 +0530

    Delete utils directory

--- utils/preprocess.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from PIL import Image
-
-def preprocess_image(img_array, target_size=(64, 64)):
-    img = Image.fromarray(img_array).resize(target_size)
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
-    return img
